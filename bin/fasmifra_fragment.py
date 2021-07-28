74452ee8c8655e46f3f5ecf94213d3336b03c813
Francois Berenger <unixjunkie@sdf.org>
2021-07-28 14:02:42 +0900

    bin/fasmifra_fragment.py: don't rely on a common module
    makes this script single-file

--- bin/fasmifra_fragment.py
+++ bin/fasmifra_fragment.py
@@ -7,16 +7,98 @@
 # Ultra-fast generator of only valid molecules using (Deep)SMILES fragments
 
 import argparse
-import fasmifra_common as common
 import random
 import rdkit
 import sys
 import time
 
-from fasmifra_common import RobustSmilesMolSupplier
 from rdkit import Chem
 from rdkit.Chem import Descriptors
 from rdkit.Chem import RWMol
+from rdkit.Chem.AtomPairs import Pairs
+
+def RobustSmilesMolSupplier(filename):
+    with open(filename) as f:
+        for line in f:
+            smile, name = line.strip().split("\t") # enforce TAB-separated
+            try:
+                mol = Chem.MolFromSmiles(smile)
+                mol.SetProp("name", name)
+                yield (name, mol)
+            except Exception:
+                print("ERROR: cannot parse: %s" % line,
+                      file=sys.stderr, end='')
+
+def nb_heavy_atom_neighbors(a):
+    res = 0
+    for neighb in a.GetNeighbors():
+        if neighb.GetAtomicNum() != 1:
+            res += 1
+    return res
+
+def get_stereo_center_indexes(m):
+    res = {}
+    # unassigned stereo centers are not reported;
+    # use includeUnassigned=True to change
+    for i, k in Chem.FindMolChiralCenters(m):
+        res[i] = True
+    return res
+
+def type_atom(a):
+    # stereo chemistry is ignored for the moment
+    nb_pi_electrons = Pairs.Utils.NumPiElectrons(a)
+    atom_num = a.GetAtomicNum()
+    nbHA = nb_heavy_atom_neighbors(a)
+    formal_charge = a.GetFormalCharge()
+    # make this easy to parse / unambiguous
+    res = "%d,%d,%d,%d" % (nb_pi_electrons, atom_num, nbHA, formal_charge)
+    return res
+
+def log_protected_bond(debug, name, b):
+    if debug:
+        print('mol %s: protected bond %d' % (name, b.GetIdx()), file=sys.stderr)
+
+# only single bonds not in rings, no stereo bonds,
+# no bond to/from a specified stereo center (i.e. if stereo was set,
+# we protect it)
+def find_cuttable_bonds(mol, debug = False):
+    name = mol.GetProp("name")
+    stereo_center_indexes = get_stereo_center_indexes(mol)
+    for b in mol.GetBonds():
+        # protect bonds to/from a stereo center
+        i = b.GetBeginAtomIdx()
+        j = b.GetEndAtomIdx()
+        if ((stereo_center_indexes.get(i) == True) or
+            (stereo_center_indexes.get(j) == True)):
+           b.SetBoolProp("protected", True)
+           log_protected_bond(debug, name, b)
+        # protect bonds between stereo bond atoms and their stereo atoms
+        if b.GetStereo() != rdkit.Chem.rdchem.BondStereo.STEREONONE:
+            (k, l) = b.GetStereoAtoms()
+            b0 = mol.GetBondBetweenAtoms(i, k)
+            b1 = mol.GetBondBetweenAtoms(i, l)
+            b2 = mol.GetBondBetweenAtoms(j, k)
+            b3 = mol.GetBondBetweenAtoms(j, l)
+            if b0 != None:
+                b0.SetBoolProp("protected", True)
+                log_protected_bond(debug, name, b0)
+            if b1 != None:
+                b1.SetBoolProp("protected", True)
+                log_protected_bond(debug, name, b1)
+            if b2 != None:
+                b2.SetBoolProp("protected", True)
+                log_protected_bond(debug, name, b2)
+            if b3 != None:
+                b3.SetBoolProp("protected", True)
+                log_protected_bond(debug, name, b3)
+    res = []
+    for b in mol.GetBonds():
+        if ((b.GetBondType() == rdkit.Chem.rdchem.BondType.SINGLE) and
+            (not b.IsInRing()) and
+            (b.GetStereo() == rdkit.Chem.rdchem.BondStereo.STEREONONE) and
+            (b.HasProp("protected") == 0)): # HasProp returns an int... :(
+            res.append(b)
+    return res
 
 def get_name(mol):
     return mol.GetProp("name")
@@ -35,7 +117,7 @@ def index_for_atom_type(atom_types_dict, atom_type):
         return v
 
 def get_cut_bonds(frag_weight, mol):
-    cuttable_bonds = common.find_cuttable_bonds(mol)
+    cuttable_bonds = find_cuttable_bonds(mol)
     cut_bonds_indexes = [b.GetIdx() for b in cuttable_bonds]
     total_weight = Descriptors.MolWt(mol)
     nb_frags = round(total_weight / frag_weight)
@@ -76,10 +158,10 @@ def tag_cut_bonds(frag_weight, randomize, atom_types_dict, input_mol):
             b_i = mol.GetBondWithIdx(i)
             a_j = b_i.GetBeginAtom()
             j = a_j.GetIdx()
-            a_j_t = common.type_atom(mol.GetAtomWithIdx(j))
+            a_j_t = type_atom(mol.GetAtomWithIdx(j))
             a_k = b_i.GetEndAtom()
             k = a_k.GetIdx()
-            a_k_t = common.type_atom(mol.GetAtomWithIdx(k))
+            a_k_t = type_atom(mol.GetAtomWithIdx(k))
             rw_mol.RemoveBond(j, k)
             # we will replace this bond by -['*][''*]-
             # ' and '' will be isotope numbers encoding the bond
