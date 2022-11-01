ff68f3613dbe86c7e682d6a7ae051c252c3b4d95
Francois Berenger <unixjunkie@sdf.org>
2022-11-01 18:37:33 +0900

    bin/to_sparse.py: ready for tests

--- bin/to_sparse.py
+++ bin/to_sparse.py
@@ -7,14 +7,16 @@ import sys
 
 input_fn = sys.argv[1]
 
-with open(input_fn, 'w') as input:
+with open(input_fn, 'r') as input:
     for i, line in enumerate(input.readlines()):
         stripped = line.strip()
-        bitstring, target_val = stripped.split(' ')
+        bitstring_target = stripped.split(' ')
+        bitstring = bitstring_target[0]
+        target_val = bitstring_target[1]
         bits = list(bitstring)
         num_bits = len(bits)
-        assert(len(bits) == 1024)
-        print('mol_%d,%s,[' % (i, target_val, file=sys.stdout, end=''))
+        assert(len(bits) == 1024) # current use case
+        print('mol_%d,%s,[' % (i, target_val), file=sys.stdout, end='')
         # use 1-based indices; because of liblinear down the line
         for i in range(0, num_bits):
             b = bits[i]
@@ -24,4 +26,4 @@ with open(input_fn, 'w') as input:
                     print(';%d:1' % (i+1), file=sys.stdout, end='')
                 else: # i == 0
                     print('%d:1' % (i+1), file=sys.stdout, end='')
-        print(']' % (i, target_val, file=sys.stdout)) # EOL
+        print(']', file=sys.stdout) # EOL
