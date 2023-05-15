cca4a0faac1b72eb16bfcc8436342abaa1d95fde
Francois Berenger <unixjunkie@sdf.org>
2023-05-16 08:59:18 +0900

    new file:   install.sh

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sudo apt install -y opam
+opam init -y # user-install of ocaml-4.13.1 as of May 16th 2023
+pip3 install rdkit
+eval `opam config env`
+opam install --fake conf-rdkit
+opam install -y fasmifra
+
+# test install was successful
+which fasmifra_fragment.py
+which fasmifra
