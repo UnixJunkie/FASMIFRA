d972862da09eb992edeb90b177403e5c657c04c8
Francois Berenger <unixjunkie@sdf.org>
2023-05-16 09:45:00 +0900

    install.sh: should also work on Mac OS

--- install.sh
+++ install.sh
@@ -2,7 +2,8 @@
 
 set -u
 
-sudo apt install -y opam
+# Mac OS or Linux opam install
+(test -e /usr/local/bin/brew && brew install opam) || sudo apt install -y opam
 opam init -y # user-install of ocaml-4.13.1 as of May 16th 2023
 pip3 install rdkit
 eval `opam config env`
