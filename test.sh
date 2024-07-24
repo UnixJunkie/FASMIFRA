a2a439c11a139f33618fd075dc9c697e7366d512
Francois Berenger <ligand@free.fr>
2024-07-24 14:42:05 +0900

    test.sh: assume software was installed properly via opam (i.e. they have run ./install.sh)
    fasmifra_fragment.py and fasmifra (exe) should be in their PATH

--- test.sh
+++ test.sh
@@ -1,17 +1,19 @@
 #!/usr/bin/env bash
 
 set -u
-#set -x # DEBUG
+set -x # DEBUG
 
 # compile the exe
-make
+# if people have executed ./install.sh; then the software is already installed
+# and should be in their PATH
+#make 
 
 # get a molecular training set
 xzcat data/CHEMBL_100k.smi.xz | head -1000 > chembl_1k.smi
 
 # fragment them
-./bin/fasmifra_fragment.py -i chembl_1k.smi -o chembl_1k_frags.smi
+fasmifra_fragment.py -i chembl_1k.smi -o chembl_1k_frags.smi
 
 # generate molecules from those fragments
 # -f: overwrite fragments cache, if any
-_build/default/src/fasmifra.exe -f -n 1000 -i chembl_1k_frags.smi -o gen_1k.smi
+fasmifra -f -n 1000 -i chembl_1k_frags.smi -o gen_1k.smi
